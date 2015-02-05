4affa78fe108e117a54d906f468b4fd6f5f552da
Kyle <kstuff@uchicago.edu>
2015-02-04 18:10:08 -0600

    I fixed muy repo

---
+++ assignment4/create_permits_hydepark_csv.sh
@@ -0,0 +1,2 @@
+wc permits.csv -l
+grep "Hyde Park" permits.csv > permits_hydepark.csv
---
+++ assignment4/exl.py
@@ -0,0 +1,12 @@
+#exl.py
+
+PI = 3.1416
+
+def sum(lst):
+	tot = lst[0]
+	for value in hlst[1:]:
+		tot += value
+	return tot
+
+2 = [0,1,2,3]
+print sum(2), PI
